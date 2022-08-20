797ff2f724f7321bb180eef85373ad43e53e8378
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2022-08-20 22:42:57 +0200

    docs: added comments to the dart code

--- lib/database_service.dart
+++ lib/database_service.dart
@@ -4,28 +4,47 @@ import 'package:flutter_widgetkit/flutter_widgetkit.dart' show WidgetKit;
 import 'package:path/path.dart' show join;
 import 'package:sqflite/sqflite.dart';
 
+/// Database Service for SQLite database, very similar to Swift's one:
+/// https://github.com/tsinis/flutter_widgetkit/blob/main/ios/CounterWidget/DatabaseService.swift
+///
+/// Dart is using underscore for private fields, Swift is using private keyword.
 class DatabaseService {
-  const DatabaseService._(this._database);
+  /// Private constructor to prevent instantiation without calling
+  /// async `openDb()` since Dart doesn't have async factories/constructors.
+  const DatabaseService._(this._db);
 
-  final Database _database;
+  /// SQLite database to use, equivalent to Swift's OpaquePointer `db` variable.
+  final Database _db;
 
+  /// Few constants to use in the database, the same as
+  /// in Swift version of this class.
   static const _appGroupId = 'group.com.example.flutterWidgetkit';
   static const _dbPath = 'database.db';
   static const _queryStatementInt = "SELECT value FROM counter;";
-  
+
+  /// Those are not referenced in the Swift version of this class,
+  /// because they are used for database creation and updating.
   static const _id = 'id';
   static const _table = 'counter';
   static const _value = 'value';
 
+  /// Same method as in Swift class, reads a value from the database,
+  /// with a raw SQL query and checking existence of the "count" value.
+  /// In Swift version we are returning String since we are showing it
+  /// as Text on home-screen widget, but in Dart we are returning integer
+  /// since we are increasing it with a taps on the button.
   Future<int?> getCount() async {
-    final map = await _database.rawQuery(_queryStatementInt);
+    final map = await _db.rawQuery(_queryStatementInt);
     if (map.isEmpty) return null;
     final maybeCount = map.first[_value];
 
     return maybeCount is int ? maybeCount : null;
   }
 
-  static Future<DatabaseService> get openDb async {
+  /// Same method as in Swift class, also opens a database from app group
+  /// directory and checking existence of the database. On the Dart side
+  /// we are also creating the database if it's not exists (on the first run).
+  static Future<DatabaseService> openDb() async {
     final directory = await AppGroupDirectory.getAppGroupDirectory(_appGroupId);
     if (directory == null) throw Exception('App Group $_appGroupId not found!');
     final database = await openDatabase(
@@ -44,13 +63,17 @@ class DatabaseService {
     return DatabaseService._(database);
   }
 
+  /// This method is only provided on Dart side, since Swift's home-screen
+  /// widget is in "read-only" mode and doesn't allow to update the count.
   Future<void> updateCount(int count) async {
-    await _database.insert(
+    /// Insert new count to the database, if exists - just replaces it.
+    await _db.insert(
       _table,
       {_id: _id, _value: count},
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
 
+    /// This will trigger a rebuild of the Swift's home-screen widget.
     return WidgetKit.reloadAllTimelines();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,16 @@ import 'package:flutter/foundation.dart' show AsyncValueSetter;
 
 import 'database_service.dart';
 
+/// Entry point for any Flutter application.
 Future<void> main() async {
+  /// Need this one to initialize the database, but since DB service have no
+  /// idea when and from where it's being started, it's a good place use it.
   WidgetsFlutterBinding.ensureInitialized();
-  final db = await DatabaseService.openDb;
+
+  /// Opens a database and returns a [DatabaseService] instance to store values.
+  final db = await DatabaseService.openDb();
+
+  /// Run our app and provide initial count with save to DB callback.
   runApp(
     CupertinoApp(
       home: MyHomePage(await db.getCount(), onUpdate: db.updateCount),
@@ -13,33 +20,47 @@ Future<void> main() async {
   );
 }
 
+/// Home page with a counter and a button to update it, similar to default one.
 class MyHomePage extends StatefulWidget {
-  const MyHomePage(this.count, {required this.onUpdate, super.key});
+  const MyHomePage(this._storedCount, {required this.onUpdate, super.key});
 
-  final int? count;
+  /// Initial tap count to display, that might be stored in DB.
+  final int? _storedCount;
+
+  /// Callback to update the count in the DB.
   final AsyncValueSetter<int> onUpdate;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+/// State for [MyHomePage] to manage the counter and the button.
 class _MyHomePageState extends State<MyHomePage> {
+  /// Default text style for all texts on the page.
   static const _style = TextStyle(color: CupertinoColors.systemGrey);
 
+  /// Current taps count to display.
   int _counter = 0;
 
   @override
   void initState() {
     super.initState();
-    _counter = widget.count ?? 0;
+
+    /// Set initial count from database if it's available.
+    _counter = widget._storedCount ?? 0;
   }
 
+  /// Increments the counter and updates the DB, as in default counter app,
+  /// but with async callback to save/update tap counts in database.
   Future<void> _incrementCounter() {
+    /// Update count on UI.
     setState(() => _counter++);
 
+    /// Update count in database.
     return widget.onUpdate(_counter);
   }
 
+  /// iOS-style page with counter and button to increment it.
   @override
   Widget build(BuildContext context) => CupertinoPageScaffold(
         navigationBar: const CupertinoNavigationBar(
